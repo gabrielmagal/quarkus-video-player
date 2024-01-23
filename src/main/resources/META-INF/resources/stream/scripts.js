8241cf333793d7ba866aee11499a88d7fb30075e
Gabriel Almeida <41032795+gabrielmagal@users.noreply.github.com>
2024-01-23 02:05:51 -0300

    Melhoria VIsual

--- src/main/resources/META-INF/resources/stream/scripts.js
+++ src/main/resources/META-INF/resources/stream/scripts.js
@@ -1,57 +1,68 @@
-document.addEventListener('DOMContentLoaded', function () {
-    getFileList();
-});
-
 function streamMedia() {
     const mediaName = document.getElementById('mediaNameInput').value;
     const folderPath = document.getElementById('folderInput').value;
     const videoPlayer = document.getElementById('videoPlayer');
 
     fetch(`/videos/${mediaName}?folder=${folderPath}`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Video not found');
-        }
-        return response.blob();
-    })
-    .then(blob => {
-        const videoUrl = URL.createObjectURL(blob);
-        videoPlayer.src = videoUrl;
-    })
-    .catch(error => {
-        console.error('Error streaming video:', error);
-    });
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    $('#errorModalBody').text('Vídeo não encontrado.');
+                    $('#errorModal').modal('show');
+                } else {
+                    $('#errorModalBody').text('Erro na requisição.');
+                    $('#errorModal').modal('show');
+                }
+                throw new Error('Erro na requisição.');
+            }
+
+            return response.blob();
+        })
+        .then(blob => {
+            const videoUrl = URL.createObjectURL(blob);
+            videoPlayer.src = videoUrl;
+        })
+        .catch(error => {
+            console.error('Error streaming video:', error);
+        });
 }
 
 function getFileList() {
     fetch(`/videos?folder=${document.getElementById('folderInput').value}`)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Failed to fetch file list');
+                $('#errorModalBody').text('Erro ao obter lista de arquivos.');
+                $('#errorModal').modal('show');
+                throw new Error('Erro ao obter lista de arquivos.');
             }
             return response.text();
         })
         .then(data => {
-            console.log('Data received:', data);
-
-            document.getElementById("fileList").innerHTML = "";
+            const fileListContainer = document.getElementById('fileList');
+            fileListContainer.innerHTML = '';
 
             const fileList = data.replace('[', '').replace(']', '').split(',').map(file => file.trim());
 
-            var table = document.createElement("table");
-            table.className = "file-table";
-
             fileList.forEach((file, index) => {
-                var row = table.insertRow();
-                var indexCell = row.insertCell(0);
-                indexCell.textContent = index + 1;
-                var fileCell = row.insertCell(1);
-                fileCell.textContent = file;
+                const listItem = document.createElement('li');
+                listItem.className = 'list-group-item list-group-item-dark d-flex justify-content-between align-items-center';
+
+                const fileName = document.createTextNode(file);
+
+                const indexBadge = document.createElement('span');
+                indexBadge.className = 'badge badge-primary badge-pill';
+                indexBadge.textContent = index + 1;
+
+                listItem.appendChild(fileName);
+                listItem.appendChild(indexBadge);
+
+                fileListContainer.appendChild(listItem);
             });
 
-            document.getElementById("fileList").appendChild(table);
+            $('#successModalBody').text('Lista de arquivos obtida com sucesso!');
+            $('#successModal').modal('show');
         })
         .catch(error => {
-            console.error('Error getting file list:', error);
+            console.error('Erro ao obter lista de arquivos:', error);
         });
 }
